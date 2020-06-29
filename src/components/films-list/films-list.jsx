b49bf159de994a8afe2f354bd6a1271952132e11
Lera Bilshchuk <leratheone@gmail.com>
2020-06-29 09:37:01 +0300

    Изменяет FilmsList для разрешения конфликта

---
+++ src/components/films-list/films-list.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import React, {PureComponent} from "react";
+import FilmCard from "../film-card/film-card.jsx";
+
+class FilmList extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {index: null};
+
+    this._handleHover = this._handleHover.bind(this);
+  }
+
+  _handleHover(value) {
+    this.setState({index: value});
+  }
+
+  render() {
+    const {films, onClick} = this.props;
+
+    return (
+      films.map((film, index) =>
+        <FilmCard
+          film={film}
+          index={index}
+          key={film.title + index}
+          onClick={onClick}
+          onHover={this._handleHover}
+        />)
+    );
+  }
+}
+
+FilmList.propTypes = {
+  films: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default FilmList;
