bccf6437d395cffab11d1b3988ca89669ccc2d5a
Lera Bilshchuk <leratheone@gmail.com>
2020-07-07 16:04:05 +0300

    Рефакторит FilmsList

--- src/components/film-list/films-list.jsx
+++ src/components/film-list/films-list.jsx
@@ -1,36 +1,21 @@
 import FilmCard from "../film-card/film-card.jsx";
 import PropTypes from "prop-types";
-import React, {PureComponent} from "react";
+import React from "react";
 import withFilmCard from "../../hocs/with-timer-on-hover/with-timer-on-hover.js";
 
 const FilmCardWrapped = withFilmCard(FilmCard);
 
-class FilmsList extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {id: null};
-
-    this._handleHover = this._handleHover.bind(this);
-  }
-
-  _handleHover(value) {
-    this.setState({id: value});
-  }
-
-  render() {
-    const {films} = this.props;
-
-    return (
-      <div className="catalog__movies-list">
-        {films.map((film) =>
-          <FilmCardWrapped
-            film={film}
-            key={film.id}
-          />)}
-      </div>
-    );
-  }
+const FilmsList = (props) => {
+  const { films } = props;
+  return (
+    <div className="catalog__movies-list">
+      {films.map((film) =>
+        <FilmCardWrapped
+          film={film}
+          key={film.id}
+        />)}
+    </div>
+  );
 }
 
 FilmsList.propTypes = {
